8a79feea6b692af7752307e59652814106d3be8e
Dog-Broad <rushyendra.7004@gmail.com>
2024-03-08 09:39:40 +0530

    Try selenium

--- src/main/python/scrape_leetcode.py
+++ src/main/python/scrape_leetcode.py
@@ -1,8 +1,12 @@
-import cloudscraper
-import urllib.parse
+import os
 import time
 import sqlite3
+import urllib.parse
+import json
+import requests
 from ratelimiter import RateLimiter
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
 
 def fetch_true_leetcode_handles(db_name):
     true_leetcode = []
@@ -40,10 +44,46 @@ def scrape_leetcode(true_leetcode):
     counter = 1
     size = len(true_leetcode)
 
-    scraper = cloudscraper.create_scraper()
-    
     # Rate limit the function to a maximum of 2 requests per second
-    limiter = RateLimiter(max_calls=MAX_REQUESTS_PER_MINUTE, period=1)
+    limiter = RateLimiter(max_calls=MAX_REQUESTS_PER_SECOND, period=1)
+
+    # Create chrome options
+    options = uc.ChromeOptions()
+    options.add_argument("--auto-open-devtools-for-tabs")
+
+    # Configure undetected-chromedriver to run in headless mode
+    driver = uc.Chrome(options=options)
+
+    driver.get("https://github.com/login")
+    time.sleep(5)
+    # find element by name login
+    login = driver.find_element(By.NAME, "login")
+    # find element by name password
+    password = driver.find_element(By.NAME, "password")
+    # find element by id signin_btn
+    signin_btn = driver.find_element(By.NAME, "commit")
+    # load username from environment variable
+    username = os.environ.get("USERNAME")
+    # load password from environment variable
+    passwd = os.environ.get("PASSWD")
+    # send username to login
+    login.send_keys(username)
+    # send password to password
+    password.send_keys(passwd)
+    # click on signin_btn
+    signin_btn.click()
+
+    # Open new tab to https://leetcode.com/accounts/login/
+    driver.get('https://leetcode.com/accounts/github/login/?next=%2F')
+    time.sleep(5)
+    try:
+        # Clock button with name authorize
+        authorize_btn = driver.find_element(By.NAME, "authorize")
+        # Click on authorize button
+        authorize_btn.click()
+        time.sleep(5)
+    except Exception as e:
+        print(f"Error: {e}")
 
     for handle, leetcode_handle in true_leetcode:
         # Construct URL for API request
@@ -52,20 +92,15 @@ def scrape_leetcode(true_leetcode):
         url = url.replace(" ", "%20")
         try:
             with limiter:
+                headers = None
                 print("URL:", url)
-                response = scraper.get(url)
-
-                # Handle specific response codes
-                if response.status_code == 404 or response.status_code == 400:
-                    if response.status_code == 524:
-                        # Wait and retry for response code 524
-                        time.sleep(30)
-                        continue
-                    else:
-                        raise RuntimeError("Error fetching data, status code:", response.status_code)
+                driver.get(url)
 
                 # Parse JSON response
-                json_content = response.json()
+                json_content = driver.find_element(By.TAG_NAME, "pre").text
+
+                # convert JSON CONTENT TO JSON PARSEABLE OBJECT
+                json_content = json.loads(json_content)
 
                 try:
                     # Get rating from JSON response
@@ -75,13 +110,13 @@ def scrape_leetcode(true_leetcode):
                     print(f"Rating for {handle} with leetcode handle {leetcode_handle} not found.")
                     rating = 0
 
+                rating = round(float(rating))
                 # Print rating information
                 print(f"({counter}/{size}) Leetcode rating for {handle} with leetcode handle {leetcode_handle} is: {rating}")
 
                 # Write to text file
                 with open("leetcode_ratings.txt", "a") as file:
-                    # round(rating) is used to round off the rating to the nearest integer
-                    file.write(f"{handle},{leetcode_handle},{round(rating)}\n")
+                    file.write(f"{handle},{leetcode_handle},{rating}\n")
 
                 counter += 1
 
@@ -95,7 +130,7 @@ def scrape_leetcode(true_leetcode):
 LEETCODE_URL = '''
 https://leetcode.com/graphql?query=query{userContestRanking(username:"{<username>}"){rating}}
 '''
-MAX_REQUESTS_PER_MINUTE = 2
+MAX_REQUESTS_PER_SECOND = 2
 
 # Main function
 def main():
